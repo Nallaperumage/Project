ab23f3a7824a2adad81939136e32b5b93a77b000
Nallaperumage <gmadhushann@gmail.com>
2018-08-15 14:07:35 +0530

    third commit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,29 +9,39 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class UserComponent implements OnInit {
 
+  // @ViewChild(MatSidenavContainer) sidenavContainer: MatSidenavContainer;
+
   panelOpenState: boolean = false;
+  isClicked = false;
 
   mobileQuery: MediaQueryList;
+  largeQuery: MediaQueryList;
 
-  fillerNav = Array(10).fill(0).map((_, i) => `Nav Item ${i + 1}`);
+  // fillerNav = Array(10).fill(0).map((_, i) => `Nav Item ${i + 1}`);
 
-  fillerContent = Array(10).fill(0).map(() =>
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-       laboris nisi ut aliquip ex ea commodo consequat.`);
+  // fillerContent = Array(10).fill(0).map(() =>
+  //     `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+  //      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+  //      laboris nisi ut aliquip ex ea commodo consequat.`);
 
   private _mobileQueryListener: () => void;
 
   
   constructor( private auth: AuthenticationService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher ) { 
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQuery = media.matchMedia('(max-width: 767px)');
+    this.largeQuery = media.matchMedia('(min-width: 767px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+    this.largeQuery.addListener(this._mobileQueryListener);
+    // this.sidenavContainer.scrollable.elementScrolled().subscribe(() => /* react to scrolling */);
   }
 
 
   ngOnInit() {
-    
   }
 
+  hideArrow(){
+    this.isClicked = !this.isClicked;
+  }
+  
 }
